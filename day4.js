9b0ca5373109ec76f2774882dab2bd427db61a0c
Aidan Murray <iiiaiid4niii@gmail.com>
2025-12-04 17:54:43 +0000

    added notes as a test ive learned stuff

--- day4.js
+++ day4.js
@@ -51,3 +51,25 @@ function checkRecurse(grid, dirs){
 }
 checkRecurse(arr, directions)
 console.log(paperCount)
+
+
+/* basic logic (for my own proof of knowledge)
+
+Create a directions lookup array that contains relation coords for each position to check. e.g the current char is 0,0
+the input is split into each line and then an array (mainly for part 2)
+loop through each row while the row number is less than the length of the "grid" e.g the amount of rows [[row], [row2]]
+nested loop through each column while column value is less than the current row length
+    assign the current char which is the input[row][col] e.g input[1][2].   
+    if the current char is not the one we are looking for, break the loop
+    if it is, we need to check each direction
+    so loop through the directions array and calculate the new row and column values to check e.g if the current pos is row 5, colum 2, it will calculate the new post to check for [1, -1] as row 6 column 1, diagonal left down of the current pos
+    check that value is the paper (only if the new row and columns actually exist) e.g row > 0 and less than the amount of rows, and column is > 0 and less than the amount of chars in the row
+    if it is, increment neighbour count
+then if the neighbour count for a specific char is less than 4, add its coords to the remove arr
+
+after this has done the check for every character loop through the to remove arrray and replace the values in those positions with . to signal the paper has gone
+
+Wrap this all in a function and only execute it while true, break condition of the removed count from one pass being 0 
+
+
+*/
